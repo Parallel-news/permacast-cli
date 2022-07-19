386f8b8662432b5a5adc8881fd6bc18e5c024800
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-07-19 12:55:29 +0300

    fix: replace file

---
+++ src/bin/handlers/account.js
@@ -0,0 +1,27 @@
+import { getArBalance } from "../../utils/arweave.js";
+import { logCyan, logRed } from "../../utils/colors.js";
+import { hasSavedKeyfile, getConfig } from "../../utils/config.js";
+import { canUploadSize } from "arweave-fees.js";
+
+export async function account() {
+  try {
+    if (!(await hasSavedKeyfile())) {
+      logRed(`ERROR: you have to save your keyfile first`);
+      process.exit(1);
+    }
+
+    const userAddress = (await getConfig("address")).address;
+    const arBalance = await getArBalance(userAddress);
+    const uploadSize = await canUploadSize(arBalance * 1e12);
+
+    logCyan(`--> address: ${userAddress}`);
+    logCyan(`--> Arweave balance: ${arBalance} $AR`);
+    logCyan(`--> Uploading size limit: ~ ${parseFloat(uploadSize).toFixed(2)} MiB`);
+
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    logRed(`ERROR: please check your internet connection`);
+    process.exit(1);
+  }
+}

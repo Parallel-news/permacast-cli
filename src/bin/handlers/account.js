ce8bd6058605b2fc77fd23dc8dde5321b425e872
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-08-17 19:25:14 +0300

    feat: display current gateway in profile

--- src/bin/handlers/account.js
+++ src/bin/handlers/account.js
@@ -1,6 +1,7 @@
 import { getArBalance } from "../../utils/arweave.js";
 import { logCyan, logRed } from "../../utils/colors.js";
 import { hasSavedKeyfile, getConfig } from "../../utils/config.js";
+import { getCurrentGateway } from "./gateways.js";
 import { canUploadSize } from "arweave-fees.js";
 
 export async function account() {
@@ -13,9 +14,11 @@ export async function account() {
     const userAddress = (await getConfig("address")).address;
     const arBalance = await getArBalance(userAddress);
     const uploadSize = await canUploadSize(arBalance * 1e12);
+    const gateway = await getCurrentGateway();
 
     logCyan(`--> address: ${userAddress}`);
     logCyan(`--> Arweave balance: ${arBalance} $AR`);
+    logCyan(`--. Arweave gateway: "${gateway}" | port: 443`)
     logCyan(`--> Uploading size limit: ~ ${parseFloat(uploadSize).toFixed(2)} MiB`);
 
     process.exit(0);

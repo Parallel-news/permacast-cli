93582a1774a61272473b617704ca4e8feabe8f95
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-09-15 01:58:08 +0300

    fix: add missing export

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const PERMACAST_API_ENDPOINT = `https://whispering-retreat-94540.herokuapp.com`;
 export const NFT_ADDRESS = `-xoIBH2TxLkVWo6XWAjdwXZmTbUH09_hPYD6itHFeZY`;
+export const TPWT_CONTRACT_ADDRESS = `mlY2wrx3jrS4F8iDPmdb4n9qmJebb1q5JDoaCTxDOSQ`;
